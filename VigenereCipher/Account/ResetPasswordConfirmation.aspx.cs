07aa916e20d9c18d33fba0befbd27f37cd24a95f
patr9240 <patr9240@gmail.com>
2017-05-28 20:32:01 -0400

    Add project files.

---
+++ VigenereCipher/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace VigenereCipher.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ VigenereCipher/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VigenereCipher.Global" Language="C#" %>
